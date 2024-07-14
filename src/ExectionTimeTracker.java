ab3044e2f9a37075834c84c39a0543f24e4480a3
ashutosh rajput <rajputashutosh81@gmail.com>
2024-07-14 23:31:47 +0530

    Global Trend

---
+++ src/ExectionTimeTracker.java
@@ -0,0 +1,24 @@
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+
+@Aspect
+@Component
+public class ExectionTimeTracker {
+    Logger logger= LoggerFactory.getLogger(ExectionTimeTracker.class);
+    @Around("@annotation(com.Ashutosh.ReportGenerator.Validation.TrackExecutiontime)")
+    public Object tracktime(ProceedingJoinPoint pjp) throws Throwable {
+        long starttime=System.currentTimeMillis();
+        Object obj=pjp.proceed();
+        long endtime=System.currentTimeMillis();
+        logger.info("Method name "+pjp.getSignature()+"time taken to execute: "+(endtime-starttime));
+        return obj;
+
+    }
+
+}
